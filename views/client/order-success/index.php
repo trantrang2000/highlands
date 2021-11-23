eb6977b809513a7fe5a0e806912feb0d755eb6b9
Anh121100 <Anhlt.gtvt@gmail.com>
2021-11-23 14:49:11 +0700

    fe create page order success

---
+++ views/client/order-success/index.php
@@ -0,0 +1,15 @@
+<title>Đặt hàng thành công</title>
+<script src="public/js/order-success.js" defer></script>
+
+<section class="container py-4">
+  <div class="border p-3 mb-3">
+    <h1 class="fs-3">
+      <i class="bi bi-check-lg pe-2 text-success"></i>Cảm ơn bạn đã mua hàng tại Highland
+    </h1>
+    <p class="text-muted">Đơn hàng của bạn đã được tiếp nhận. Bạn có thể xem tại <a href="my/orders">Đơn hàng của tôi</a>. Thông tin chi tiết về đơn hàng đã được gửi đến địa chỉ email của bạn.</p>
+  </div>
+
+  <div class="text-center">
+    <a href="product" class="btn btn-primary text-white">Tiếp tục mua sắm</a>
+  </div>
+</section>
